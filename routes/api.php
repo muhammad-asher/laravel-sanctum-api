528ed9d52c7c51aff5eb7f6326da7724fd1aef37
muhammad-asher <muhammadasher.se@gmail.com>
2023-01-26 20:49:53 +0500

    Sanctum register,login,logout user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -20,9 +21,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products',ProductController::class);
+//Route::resource('products',ProductController::class);
+
+// Public Routes
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
 Route::get('/products/search/{name}',[ProductController::class,'search']);
-//Route::get('/products',[ProductController::class,'index']);
-//
-//Route::post('/products',[ProductController::class,'store']);
+Route::get('/products',[ProductController::class,'index']);
+Route::get('/products/{id}',[ProductController::class,'show']);
+
+// Protected Routes
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('/products/{id}',[ProductController::class,'update']);
+    Route::delete('/products/{id}',[ProductController::class,'destroy']);
+    Route::post('/logout',[AuthController::class,'logout']);
+
+});
+
 
